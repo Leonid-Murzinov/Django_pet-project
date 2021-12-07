cd83fa03092602c871efa89d0aae6db169ed87ce
Leonid-Murzinov <leonid.murzinov@gmail.com>
2021-12-07 13:44:37 +0300

    Добавил логин

--- djangopet/djangopet/settings.py
+++ djangopet/djangopet/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'djangopet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangopet',
+        'USER': 'admin_test',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
---
+++ djangopet/users/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
+class LoginView(LoginView):
+    template_name = 'users/login.html'
